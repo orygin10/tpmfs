23d92a0d7941db4a48ae045f537389f2f895b20e
Maxime <user@mail.com>
2018-05-07 12:49:14 +0200

    Update tpm.sh Setup GETOPT to call API functions

--- tpm.sh
+++ tpm.sh
@@ -18,6 +18,78 @@ source "${CUR_DIR}/common.sh"
 # - _check
 source "${CUR_DIR}/api.sh"
 
+# Getopt
+while getopts ":drw:ui:hp:" opt; do
+  case $opt in
+    d)
+      # Define
+      BOOL_DEFINE=1
+      ;;
+    r)
+      # Read 2048 Bytes # Need file
+      BOOL_READ=1
+      ;;
+    w)
+      # Write 2048 bytes # Need data
+      INPUT_DATA="${OPTARG}"
+      ;;
+    u)
+      # Release
+      BOOL_RELEASE=1
+      ;;
+    p)
+      OWNERPASS="${OPTARG}"
+      ;;
+    i)
+      INDEX="${OPTARG}"
+      ;;
+    h)
+      BOOL_HELP=1
+      ;;
+    \?)
+      printf "Invalid option: -%s\n" "$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      printf "Option -%s requires an argument.\n" "$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+function print_usage {
+  printf "Usage: %s -[ s|r|w|u ] -i INDEX [-d DATA] \n" "$0" >&2
+  printf "   -s    Define INDEX with attribute WORD on TPM Chip\n" >&2
+  printf "   -r    Read 2048 bytes from INDEX\n" >&2
+  printf "   -w    Write DATA (up to 2048 bytes) to INDEX\n" >&2
+  printf "   -u    Release INDEX\n" >&2
+  exit 1
+}
+
+[ "$#" -eq 0 ] || [ ! -z ${BOOL_HELP+x} ] \
+  && print_usage && exit 1
+
+[ -z ${OWNERPASS+x} ] && OWNERPASS="$(_askpass)" || exit 1
+[ -z ${SIZE+x} ] && SIZE=2048
+
+[ ! -z ${BOOL_DEFINE+x} ] && [ ! -z ${INDEX+x} ] \
+  && _define \
+  && exit 0
+
+[ ! -z ${INPUT_DATA+x} ] && [ ! -z ${INDEX+x} ] \
+  && _write \
+  && exit 0
+
+[ ! -z ${BOOL_READ+x} ] && [ ! -z ${INDEX+x} ] \
+  && _read \
+  && exit 0
+
+[ ! -z ${BOOL_RELEASE+x} ] && [ ! -z ${INDEX+x} ] \
+  && _release \
+  && exit 0
+
+print_usage && exit 1
+
 function send_data {
   #INDEX="0x1400001"
   #INPUT_DATA="Hi !"
@@ -34,12 +106,5 @@ function send_data {
   _check &&
   return 0 || return 1
 }
-
-# 0x1000000 à 0x1ffffff
-# 16777215 (2^24) bytes
-# 2^24 * 2048 (2^35) bytes
-# = 32MiB
-
-printf "remiliascarlet" | send_data "0x1ffffff" 2048 "Hello !"
 
 exit 0
