b07b3280576efadc9774ff36460985894fa22f70
Maxime <user@mail.com>
2018-05-08 16:10:06 +0200

    FS() class Better tests using unittest Use of a tmp/ directory instead of putting temporary files in root directory

--- fs/fs.py
+++ fs/fs.py
@@ -1,45 +1,97 @@
 #!/usr/bin/env python2
-
-import yaml
-
-from filetable import Filetable
-from interface import Interface
+from filetable import Filetable, FTError
+from interface import Interface, InterfaceError
 import os
+import sys
+import yaml
+import unittest
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def test():
-    # Nuke filetable
-    with open('filetable.yml', 'w') as f:
-        f.write('files:\n')
+class FS:
+    def __init__(self):
+        self.i = Interface()
+        try:
+            yml = self.i.pull_offset(0)
+        except InterfaceError:
+            self.write_to_tmp("files:\n", "filetable.yml")
+            self.i.push_offset(0, "{0}/tmp/filetable.yml".format(dir_path))
+            yml = self.i.pull_offset(0)
 
-    with Filetable() as ft:
+        self.ft = Filetable(yml)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+    ############################
+
+    def write_to_tmp(self, data, filename):
+        with open("{0}/tmp/{1}".format(dir_path, filename), "w") as fd:
+            fd.write(data)
+        return True
+
+class Tests(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.i = Interface()
+
+    def setUp(self):
+        # Start with an empty TPM chip
+        try:
+            self.i.release_offset(0)
+        except InterfaceError:
+            pass
+        
+    def test_1_pull_filetable_error(self):
+        # Should raise bc there is no filetable in index 0
+        with self.assertRaises(InterfaceError):
+            self.i.pull_offset(0)
+
+    def test_2_pull_filetable(self):
+       # Should not raise InterfaceError 
+        with open('{0}/tmp/filetable.yml'.format(dir_path), 'w') as fd:
+            fd.write('files:\n')
+        self.i.push_offset(0, '{0}/tmp/filetable.yml'.format(dir_path))
+        try:
+            self.i.pull_offset(0)
+        except InterfaceError:
+            self.fail()
+
+
+def tests():
+    with FS() as fs:
+        assert fs.i.pull_offset(0) != False, "Pulling offset 0 should not return False"
+
+        # Pull filetable into yml_filetable
+        filetable = fs.i.pull_offset(0)
+        yml_filetable = yaml.load(filetable)
+
+        yaml.dump(yml_filetable, open('{0}/tmp/filetable.yml'.format(dir_path), 'w'))
+        assert fs.i.push_offset(0, '{0}/tmp/filetable.yml'.format(dir_path)), "Pushing filetable file to offset 0 should return True"
+
         # Test list_files() with empty table
-        assert not ft.list_files(), "Listing all files should return False if table is empty"
+        assert fs.ft.list_files(), "Listing all files should return False if table is empty"
 
         # Test add_file
-        assert ft.add_file('data4.txt', 2004), "Adding a non-existing file should return True"
-        assert not ft.add_file('data4.txt', 2004), "Adding an already existing file should return False"
+        assert fs.ft.add_file('data4.txt', 2004), "Adding a non-existing file should return True"
+        assert not fs.ft.add_file('data4.txt', 2004), "Adding an already existing file should return False"
 
         # Test list_files
-        assert ft.list_files(), "Listing all files should return True"
+        assert fs.ft.list_files(), "Listing all files should return True"
 
         # Test remove_file
-        assert ft.remove_file('data4.txt'), "Removing an existing file should return True"
-        assert not ft.remove_file('data4.txt'), "Removing a non-existing file should return False"
+        assert fs.ft.remove_file('data4.txt'), "Removing an existing file should return True"
+        assert not fs.ft.remove_file('data4.txt'), "Removing a non-existing file should return False"
 
         # Test get_metadata
-        assert not ft.get_metadata('data.txt'), "Getting metadata for an non-existing file should return False"
-        ft.add_file('data.txt', 200)
-        assert ft.get_metadata('data.txt'), "Getting metadata for an existing file should not return False"
-
-    with Interface() as i:
-        i.push_offset(0, "{}/filetable.yml".format(dir_path))
-        filetable = i.pull_offset(0)
-        print yaml.load(filetable)
+        assert not fs.ft.get_metadata('data.txt'), "Getting metadata for an non-existing file should return False"
+        fs.ft.add_file('data.txt', 200)
+        assert fs.ft.get_metadata('data.txt'), "Getting metadata for an existing file should not return False"
 
 def main():
-    test()
+    unittest.main()
 
 if __name__=='__main__':
     main()
