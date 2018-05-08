c0de0706b0953bd670fac6ce23f4a597b2d51629
Maxime <user@mail.com>
2018-05-08 16:07:19 +0200

    Improved bash() and __init__() Added method release offset Added exception InterfaceException

--- fs/interface.py
+++ fs/interface.py
@@ -1,30 +1,66 @@
 #!/usr/bin/env python2
 from subprocess import Popen, PIPE
 import os
+import sys
+import re
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def bash(bashCommand):
-    process = Popen(bashCommand.split(), stdout=PIPE)
+def bash(bashCommand, debug = False):
+    """Returns a tuple containing shell STDOUT and STATUS_CODE"""
+    bashCommand = "{0}/../".format(dir_path) + bashCommand
+    if debug:
+        bashCommand = "bash -x " + bashCommand
+        process = Popen(bashCommand.split(), stdout=PIPE)
+    else:
+        process = Popen(bashCommand.split(), stdout=PIPE, stderr=PIPE)
+
     stdout, _ = process.communicate()
-    return stdout
+    return stdout, process.returncode
+
+
+class InterfaceError(Exception):
+    pass
 
 
 class Interface:
     def __init__(self):
-        self.password = raw_input("Enter password: ")
+        sys.stderr.write('Enter password : ')
+        sys.stderr.flush()
+        self.password = raw_input()
+        sys.stderr.write('\n')
+        sys.stderr.flush()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    ##########################
 
     def push_offset(self, offset, file_path):
         """Write 2048-bytes file into TPM at chosen offset"""
+        try:
+            with open(file_path, "r") as fd:
+                if fd.read() is None:
+                    raise InterfaceError("File {0} is empty".format(file_path))
+        except IOError:
+            raise InterfaceError("File {0} does not exist".format(file_path))
+
         start = 1500000
-        print bash("{0}/../tpm.sh -ui 0x{1} -p {2}".format(dir_path, start+offset, self.password))
-        print bash("{0}/../tpm.sh -di 0x{1} -p {2}".format(dir_path, start+offset, self.password))
-        print bash("{0}/../tpm.sh -i 0x{1} -p {2} -f {3}".format(dir_path, start+offset, self.password, file_path))
+        bash("tpm.sh -di 0x{0} -p {1}".format(start+offset, self.password))
+        _, rc = bash("tpm.sh -i 0x{0} -p {1} -f {2}".format(start+offset, self.password, file_path))
+        if rc == 0:
+            return True
+        raise InterfaceError("Cannot push to offset {0}".format(offset))
 
     def pull_offset(self, offset):
         """Write 2048-bytes file into TPM at chosen offset"""
         start = 1500000
-        data = bash("{0}/../tpm.sh -i 0x{1} -p {2} -r".format(dir_path, start+offset, self.password))
-        return str(data).replace('\0', '')
+        data, rc = bash("tpm.sh -i 0x{0} -p {1} -r".format(start+offset, self.password))
+        if rc == 0:
+            return str(data).replace('\0', '')
+        raise InterfaceError("Cannot pull from offset {0}".format(offset))
 
     def push(self):
         """Store a file into TPM Chip"""
@@ -42,12 +78,11 @@ class Interface:
         """Nuke a file in TPM Chip"""
         pass
 
-    ############################
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        # Cleanup
-        pass
+    def release_offset(self, offset):
+        """Release an offset on TPM, raises InterfaceError if release fails"""
+        start = 1500000
+        _, rc = bash("tpm.sh -ui 0x{0} -p {1}".format(start+offset, self.password))
+        if rc == 0:
+            return True
+        raise InterfaceError("Cannot release offset {0}".format(offset))
 
